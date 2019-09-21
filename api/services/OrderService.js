22324f4c7d1073f1172e0e76adfc91a06ffa5b5b
Noitidart <noitidart@gmail.com>
2019-09-21 09:02:17 -0700

    Fill out updateOrder logic

--- api/services/OrderService.js
+++ api/services/OrderService.js
@@ -19,7 +19,19 @@ const OrderService = {
 
   getOrders: function() {},
 
-  updateOrder: function() {}
+  updateOrder: async function(orderEvent) {
+
+    const order = await Order.updateOne({ clientId: orderEvent.clientId }).set({
+      ...pick(orderEvent, 'clientId', 'name', 'destination'),
+      status: orderEvent.eventName,
+      ...orderEvent.eventName === 'COOKED' && { cookedAt: Date.now() }
+    });
+
+    await Order.addToCollection(order.id, 'events', orderEvent.id);
+
+    OrderService.broadcastOrder(order);
+
+  }
 
 };
 
